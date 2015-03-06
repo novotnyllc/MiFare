7891504698e19248bae6dca6128b69c26ff5b567
Oren Novotny <oren@novotny.org>
2015-03-06 15:00:30 -0500

    Extract types to seperate file

--- src/MiFare.Shared/Classic/MiFareStandardCardReaderBase.cs
+++ src/MiFare.Shared/Classic/MiFareStandardCardReaderBase.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -177,55 +176,5 @@ namespace MiFare.Classic
         {
             Dispose(false);
         }
-    }
-
-    [DebuggerDisplay("Sector: {Sector}, KeyType: {KeyType}")]
-    public class SectorKeySet
-    {
-        public int Sector { get; set; }
-        public KeyType KeyType { get; set; }
-        public byte[] Key { get; set; }
-
-        public bool IsValid => Sector >= 0 && Sector < 40 && Key != null && Key.Length == 6;
-    }
-
-    internal struct SectorKey : IEquatable<SectorKey>
-    {
-        public SectorKey(int sector, InternalKeyType keyType)
-        {
-            Sector = sector;
-            KeyType = keyType;
-        }
-        public bool Equals(SectorKey other)
-        {
-            return Sector == other.Sector && KeyType == other.KeyType;
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            return obj is SectorKey && Equals((SectorKey)obj);
-        }
-
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                return (Sector * 397) ^ (int)KeyType;
-            }
-        }
-
-        public static bool operator ==(SectorKey left, SectorKey right)
-        {
-            return left.Equals(right);
-        }
-
-        public static bool operator !=(SectorKey left, SectorKey right)
-        {
-            return !left.Equals(right);
-        }
-
-        public int Sector { get; }
-        public InternalKeyType KeyType { get; }
     }
 }
